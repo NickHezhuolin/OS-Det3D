fbab4b54e0e4a8b0affd958024d6b1bc29eee5f5
NickHezhuolin <1986013909@qq.com>
2023-09-26 13:28:29 +0000

    init from 100

--- projects/mmdet3d_plugin/bevformer/dense_heads/__init__.py
+++ projects/mmdet3d_plugin/bevformer/dense_heads/__init__.py
@@ -3,3 +3,4 @@ from .bev_head import BEVHead
 from .bevformer_head import BEVFormerHead
 from .owbevformer_headV1 import OWBEVFormerHeadV1
 from .owbevformer_headV1_rpn import OWBEVFormerHeadV1RPN
+from .owbevformer_headV1_rpn_wo_bbox import OWBEVFormerHeadV1RPNV1  # with out bbox refine
