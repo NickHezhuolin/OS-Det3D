8018ea32defc332e0e3ce55a5591ef7100d5870f
NickHezhuolin <1986013909@qq.com>
2023-11-01 10:17:30 +0800

    init from 248

--- projects/mmdet3d_plugin/bevformer/dense_heads/__init__.py
+++ projects/mmdet3d_plugin/bevformer/dense_heads/__init__.py
@@ -4,3 +4,4 @@ from .owbevformer_head_unk_gt import OWBEVFormerHead_UnkGT
 from .owbevformer_headV1 import OWBEVFormerHeadV1
 from .owbevformer_headV1_rpn import OWBEVFormerHeadV1RPN
 from .owbevformer_headV1_rpn_wo_bbox import OWBEVFormerHeadV1RPNV1  # with out bbox refine
+from .owbevformer_headV1_rpn_wo_ow import OWBEVFormerHeadV1RPNV1_Without_OWDETR_Select # with out owdetr select
