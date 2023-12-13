518affe46d32975932601ba4b4178d23b795d79f
NickHezhuolin <1986013909@qq.com>
2023-12-13 02:58:08 +0000

    init from 100

--- projects/mmdet3d_plugin/bevformer/dense_heads/__init__.py
+++ projects/mmdet3d_plugin/bevformer/dense_heads/__init__.py
@@ -11,6 +11,7 @@ from .owbevformer_headV1_rpn_wo_bbox_w_sw import OWBEVFormerHeadV1RPNV1_with_sof
 from .owbevformer_headV1_rpn_wo_bbox_wo_nc import OWBEVFormerHeadV1RPNV1_without_nc_branch # without nc branch
 from .owbevformer_headV1_rpn_wo_bbox_w_sw_rescale import OWBEVFormerHeadV1RPNV1_with_soft_weight_rescale # with soft weight rescale
 from .owbevformer_headV1_rpn_w_bbox_w_sw import OWBEVFormerHeadV1RPN_w_bb_with_soft_weight # with bbox refine with soft weight rescale
+from .owbevformer_headV1_rpn_wo_bbox_w_sw_wo_nc import OWBEVFormerHeadV1RPNV1_with_soft_weight_without_nc_branch
 
 # based on owbevformer_headV1_rpn_wo_bbox + new nc branch
 from .owbevformer_headV2 import OWBEVFormerHeadV2
